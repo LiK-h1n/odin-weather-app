aebbfe3859aa12109dceacb78a7c3482baf7267b
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-06-23 20:41:19 +0530

    Add easter egg

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ async function updateWeather(location) {
 }
 
 function init() {
-  updateWeather("Pathanamthitta");
+  updateWeather("Alsalmaniya");
 }
 
 searchForm.addEventListener("submit", async (event) => {
